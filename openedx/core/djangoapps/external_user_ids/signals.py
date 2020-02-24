916f2413b51ba1b2be21c2544368272589c496ef
Albert (AJ) St. Aubin <astaubin@edx.org>
2020-02-24 14:36:16 -0500

    Create MB External ID on creation of a CourseEntitlement in MB Program

--- openedx/core/djangoapps/external_user_ids/signals.py
+++ openedx/core/djangoapps/external_user_ids/signals.py
@@ -37,3 +37,29 @@ def create_external_id_for_microbachelors_program(
                 user=instance.user,
                 type_name=ExternalIdType.MICROBACHELORS_COACHING
             )
+
+
+@receiver(post_save, sender='entitlements.CourseEntitlement')
+def create_external_id_for_microbachelors_program_entitlement(
+    sender, instance, created, **kwargs  # pylint: disable=unused-argument
+):
+    """
+    Watches for post_save signal for creates on the CourseEntitlement table.
+    Generate an External ID if the Entitlement is in a MicroBachelors Program
+    """
+    if (
+        created and
+        instance.user and
+        not ExternalId.user_has_external_id(
+            user=instance.user,
+            type_name=ExternalIdType.MICROBACHELORS_COACHING)
+    ):
+        mb_programs = [
+            program for program in get_programs(course=instance.course_uuid)
+            if program.get('type_slug', '') == 'microbachelors'
+        ]
+        if mb_programs:
+            ExternalId.add_new_user_id(
+                user=instance.user,
+                type_name=ExternalIdType.MICROBACHELORS_COACHING
+            )
